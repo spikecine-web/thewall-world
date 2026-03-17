a6b526ecdb8274b7565fd87d80c92f0d727a4979
spikecine-web <spikecine@gmail.com>
2026-03-16 20:46:38 -0500

    Update TheWall.tsx

--- app/TheWall.tsx
+++ app/TheWall.tsx
@@ -2211,7 +2211,7 @@ export default function TheWall() {
             }}
           >
             We're building a globe made of{" "}
-            <strong>5 million words</strong> from from
+            <strong>5 million words</strong> from
             people all over the planet. You choose a word, a name, or a message
             — it becomes a tile on the globe forever. When we reach 5 million,
             the digital globe becomes a{" "}
